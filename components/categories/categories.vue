e8ad7d1ed218b2b96bd35a4c05a6b38786b5350d
etienne.blavo <etiennebobo17@gmail.com>
2021-11-25 19:41:48 +0530

    fix: layout

--- components/categories/categories.vue
+++ components/categories/categories.vue
@@ -1,51 +1,51 @@
 <template>
   <div>
     <div class="mb-16">
-      <div class="flex flex-wrap max-w-7xl">
+      <div class="grid md:grid-cols-4 grid-cols-2 max-w-7xl">
         <div
           v-for="(category, index) in categories"
           :key="index"
-          class="lg:pt-12 pt-8 w-full md:w-4/12 px-4 text-center"
+          class="lg:pt-12 pt-8 w-full px-4 text-center"
         >
           <router-link
             :to="{ name: 'categories-id', params: { id: category.slug } }"
           >
             <div
               class="
-                relative
-                flex flex-col
                 min-w-0
+                flex
+                items-center
+                justify-center
                 break-words
                 hover:shadow-md
                 bg-white
                 w-full
-                mb-8
                 shadow-sm
                 rounded-lg
                 border
               "
             >
-              <div class="px-4 py-5 flex-auto">
+              <div class="">
                 <div
                   class="
-                    text-white text-center
-                    inline-flex
+                    flex
                     items-center
+                    px-4
+                    py-4
+                    md:py-6
                     justify-center
-                    mb-5
                     shadow-sm
-                    bg-gray-100
                     rounded-full
                   "
                 >
                   <img
                     v-if="category.image"
-                    class="w-14 h-14 rounded-full"
+                    class="w-8 h-8 mr-4 rounded-full"
                     :src="category.image.url"
                     alt=""
                   />
+                  <p class="text-xl font-normal">{{ category.name }}</p>
                 </div>
-                <p class="text-xl font-normal">{{ category.name }}</p>
               </div>
             </div>
           </router-link>
