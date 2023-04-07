bf10668d0a0b68d766e770d8f1e1507ff7b9fa97
Efoe-Blavo <efoe.blavo@neumannfornax.com>
2023-04-07 16:13:42 +0530

    fix: list categorie display

--- components/categories/categories.vue
+++ components/categories/categories.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <div class="mb-16">
-      <div class="grid md:grid-cols-4 grid-cols-2 max-w-7xl">
+    <div class="mb-6 md:mb-12">
+      <div
+        class="
+          grid
+          md:grid-cols-6
+          lg:grid-cols-8
+          sm:grid-cols-4
+          grid-cols-3
+          max-w-7xl
+        "
+      >
         <div
           v-for="(category, index) in categories"
           :key="index"
-          class="lg:pt-12 pt-8 w-full px-4 text-center"
+          class="lg:pt-3 pt-3 w-full px-2 text-center"
         >
           <router-link
             :to="{ name: 'categories-id', params: { id: category.slug } }"
@@ -14,8 +23,6 @@
               class="
                 min-w-0
                 flex
-                items-center
-                justify-center
                 break-words
                 hover:shadow-md
                 bg-white
@@ -23,6 +30,7 @@
                 mb-6
                 md:py-0 md:mb-0
                 rounded-lg
+                justify-center
                 border
               "
             >
@@ -31,20 +39,20 @@
                   class="
                     flex
                     items-center
-                    px-4
-                    py-3
-                    md:py-6
                     justify-center
+                    px-2
+                    py-2
+                    md:py-3
                     rounded-full
                   "
                 >
                   <img
                     v-if="category.image"
-                    class="w-12 h-12 mr-4 border border-gray-200 rounded-full"
+                    class="w-8 h-8 mr-1 rounded-full"
                     :src="category.image.url"
                     alt=""
                   />
-                  <p class="md:text-xl text-sm font-normal">
+                  <p class="md:text-sm text-xs font-normal">
                     {{ category.name }}
                   </p>
                 </div>
